4631973b037a03302bc913ecab2f30f43f4d49eb
Edidiong Bassey <edidiong.bassey@frogasia.com>
2022-11-30 13:50:11 +0800

    feat(Learnstein): Speech-to-Text check correct and wrong answer

--- src/components/speechkit.js
+++ src/components/speechkit.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useSpeechRecognition } from "react-speech-kit";
 
-import { Card, Dialog, makeStyles, Icon } from "@material-ui/core";
+import { Dialog, Paper, makeStyles, Icon, IconButton } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -75,65 +75,89 @@ export default function SpeechKit({ onClose, open }) {
       open={open}
       className={classes.root}
     >
-      <form id='speech-recognition-form' style={{ width: "70%", padding: 16 }}>
-        <h2>Speech Recognition</h2>
-        {!supported && (
-          <p>
-            Oh no, it looks like your browser doesn&#39;t support Speech
-            Recognition.
-          </p>
-        )}
-        {supported && (
-          <React.Fragment>
+      <Paper style={{ display: "flex" }}>
+        <form
+          id='speech-recognition-form'
+          style={{ width: "70%", padding: 16 }}
+        >
+          <h2>Speech Recognition</h2>
+          {!supported && (
             <p>
-              {`Click 'Listen' and start speaking.
-               SpeechRecognition will provide a transcript of what you are saying.`}
+              Oh no, it looks like your browser doesn&#39;t support Speech
+              Recognition.
             </p>
-            <div style={{ display: "flex", marginBottom: 16 }}>
-              <label htmlFor='language'>Language</label>
-              <select
-                form='speech-recognition-form'
-                id='language'
-                value={lang}
-                onChange={changeLang}
-              >
-                {languageOptions.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <p style={{ fontWeight: "bold" }}>
-                Question: The ________ Army invaded Malaya in 8 December 1941
+          )}
+          {supported && (
+            <Fragment>
+              <p>
+                {`Click 'Listen' and start speaking.
+               SpeechRecognition will provide a transcript of what you are saying.`}
               </p>
-            </div>
-            {/* <label htmlFor='transcript'>Transcript</label> */}
-            <textarea
-              id='transcript'
-              name='transcript'
-              placeholder='Waiting to take notes ...'
-              value={value}
-              rows={3}
-              disabled
-              style={{ display: "flex", width: "100%" }}
-            />
-            <button disabled={blocked} type='button' onClick={toggle}>
-              {listening ? "Stop" : "Listen"}
-            </button>
-            {blocked && (
-              <p style={{ color: "red" }}>
-                The microphone is blocked for this site in your browser.
-              </p>
-            )}
-          </React.Fragment>
-        )}
-      </form>
-      <div style={{ width: "30%" }}>
-        {showCorrect ? <Icon fontSize='large'>check</Icon> : null}
-        {showWrong ? <Icon fontSize='large'>dangerous</Icon> : null}
-      </div>
+              <div style={{ display: "flex", marginBottom: 16 }}>
+                <label htmlFor='language'>Language</label>
+                <select
+                  form='speech-recognition-form'
+                  id='language'
+                  value={lang}
+                  onChange={changeLang}
+                >
+                  {languageOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <p style={{ fontWeight: "bold" }}>
+                  Question: The ________ Army invaded Malaya in 8 December 1941
+                </p>
+              </div>
+              {/* <label htmlFor='transcript'>Transcript</label> */}
+              <textarea
+                id='transcript'
+                name='transcript'
+                placeholder='Waiting to take notes ...'
+                value={value}
+                rows={3}
+                disabled
+                style={{ display: "flex", width: "100%" }}
+              />
+              <button disabled={blocked} type='button' onClick={toggle}>
+                {listening ? "Stop" : "Listen"}
+              </button>
+              {blocked && (
+                <p style={{ color: "red" }}>
+                  The microphone is blocked for this site in your browser.
+                </p>
+              )}
+            </Fragment>
+          )}
+        </form>
+        <div
+          style={{
+            width: "30%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {showCorrect ? (
+            <IconButton>
+              <Icon fontSize='large' style={{ color: "white" }}>
+                check
+              </Icon>
+            </IconButton>
+          ) : null}
+          {showWrong ? (
+            <IconButton style={{ background: "red", boxShadow: "none" }}>
+              <Icon fontSize='large' style={{ color: "white" }}>
+                close
+              </Icon>
+            </IconButton>
+          ) : null}
+        </div>
+      </Paper>
     </Dialog>
   );
 }
