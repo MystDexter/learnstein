440d6315b8b0a0bff678ac5ef7391d5707671927
Edidiong Bassey <edidiong.bassey@frogasia.com>
2022-11-30 17:37:50 +0800

    chore(Learnstein): Update text-speech text

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,1 +1,11 @@
-export const article = `The occupation commenced with Imperial Japanese Army landings at Padang Pak Amat beach Kota Bharu just after midnight on 8 December 1941, triggering a ferocious battle with the British Indian Army an hour before the attack on Pearl Harbor. This battle marked the official start of the Pacific War and the start of the Japanese occupation of Malaya. Kota Bharu airport was occupied in the morning. Sungai Patani, Butterworth, and Alor Star airports were captured on 9 December 1941. Japanese soldiers landing at Kota Bharu divided into two separate forces, with one moving down the east coast towards Kuantan, and the other southwards towards the Perak River. On 11 December 1941, the Japanese started bombing Penang. Jitra and then Alor Star fell into Japanese hands on 12 December 1941. The British had to retreat to the south. On 16 December 1941, the British left Penang to the Japanese, who occupied it on 19 December.`;
+export const article = `Two factors of Japanese Invasion in Malaysia:
+1. Exploitation of raw material resources (tin, rubber).
+2. Military strategy.
+
+Why did Japan invade Malaysia? 
+
+1. Raw materials resources exploitation 
+2. Military power in Malaya.
+
+`;
+// `The occupation commenced with Imperial Japanese Army landings at Padang Pak Amat beach Kota Bharu just after midnight on 8 December 1941, triggering a ferocious battle with the British Indian Army an hour before the attack on Pearl Harbor. This battle marked the official start of the Pacific War and the start of the Japanese occupation of Malaya. Kota Bharu airport was occupied in the morning. Sungai Patani, Butterworth, and Alor Star airports were captured on 9 December 1941. Japanese soldiers landing at Kota Bharu divided into two separate forces, with one moving down the east coast towards Kuantan, and the other southwards towards the Perak River. On 11 December 1941, the Japanese started bombing Penang. Jitra and then Alor Star fell into Japanese hands on 12 December 1941. The British had to retreat to the south. On 16 December 1941, the British left Penang to the Japanese, who occupied it on 19 December.`;
